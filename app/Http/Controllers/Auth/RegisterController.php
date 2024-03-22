f52aa9afbdf47370e3d8060149d62b0764e7fb8a
alexDev2808 <colores.2808.8000@gmail.com>
2024-03-22 14:50:14 -0600

    Insertando registro en la DB, evitando duplicados, hashear password, redireccionando

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -13,13 +16,27 @@ class RegisterController extends Controller
 
     public function store( Request $request ) {
 
+        // Modificar el Request
+        $request->request->add(['username' => Str::slug($request->username)]); // convertirlo a una url
+
         // Validacion 
         $this->validate($request, [
             'name' => 'required|min:3',
-            'username' => 'required|unique:users,name|min:3|max:20',
+            'username' => 'required|unique:users,username|min:3|max:20',
             'email' => 'required|unique:users,email|email|max:50',
-            'password'=> 'required'
+            'password'=> 'required|confirmed|min:6' // confirmed para verificar que sean iguales con password_confirmation
         ]);
+
+        User::create([
+            'name' => $request->name,
+            'username'=> $request->username,
+            'email'=> $request->email,
+            'password'=> Hash::make( $request->password ),
+        ]);
+
+
+        return redirect()->route('posts.index');
+
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 
 Route::get('/register', [RegisterController::class,'index'])->name('register'); // mostrar vista
 Route::post('/register', [RegisterController::class,'store']);
+
+Route::get('/muro', [PostController::class,'index'])->name('posts.index');
