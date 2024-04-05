9d29e51d3ca17638d0955cfa97e2d7739e6a07c8
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-05 11:55:18 -0600

    Diseño para agregar comentarios

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -25,8 +25,44 @@
             </div>
         </div>
 
-        <div class="md:w-1/2">
-            
+        <div class="md:w-1/2 p-5">
+            <div class="shadow bg-white p-5 mb-5 ">
+                <p class="text-xl font-bold text-center mb-4">Agrega un nuevo comentario</p>
+
+                <form>
+                    <div class="mb-5">
+                        <label 
+                            for="comentario"
+                            class="mb-2 block uppercase text-gray-500 font-bold" 
+                            >
+                            Comentario
+                        </label>
+                        <textarea 
+                            id="comentario"
+                            name="comentario"
+                            placeholder="Descripcion de la publicacion"
+                            class="border p-3 w-full rounded-lg
+                                @error('comentario')
+                                    border-red-500
+                                @enderror"
+                            ></textarea>
+
+                            @error('comentario')
+                                <p 
+                                    class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                                    {{ $message }}
+                                </p>
+                            @enderror
+                    </div>
+
+
+                    <input 
+                        type="submit"
+                        value="Crear comentario"
+                        class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg mt-6"
+                    >
+                </form>
+            </div>
         </div>
     </div>
 
