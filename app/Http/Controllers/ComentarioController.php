eb58d0e284ce9d2184e28329e0d91d6287a20a20
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-05 12:14:29 -0600

    Agregando el routing para guardar comentarios

---
+++ app/Http/Controllers/ComentarioController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ComentarioController extends Controller
+{
+    public function store() {
+        dd('comentando...');
+
+        //validar
+
+        //almacenar
+
+
+        //imprimir mensaje
+    }
+}
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,7 +7,7 @@
 
 @section('contenido')
 
-    <div class="container mx-auto flex">
+    <div class="container mx-auto md:flex">
         <div class="md:w-1/2">
             <img  
                 src="{{ asset('uploads' . '/' . $post->imagen ) }}"
@@ -32,7 +32,8 @@
             <div class="shadow bg-white p-5 mb-5 ">
                 <p class="text-xl font-bold text-center mb-4">Agrega un nuevo comentario</p>
 
-                <form>
+                <form method="POST" action="{{ route('comentarios.store', ['post' => $post, 'user' => $user ]) }}">
+                    @csrf
                     <div class="mb-5">
                         <label 
                             for="comentario"
