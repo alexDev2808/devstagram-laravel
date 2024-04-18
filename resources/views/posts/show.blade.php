18252c40b3ea7e7f07efaf5d0dde126ea4673b0a
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-18 11:54:12 -0600

    Eliminando publicaciones

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -23,6 +23,26 @@
                 <p class="text-sm text-gray-500">{{ $post->created_at->diffForHumans() }}</p>
                 <p class="mt-6">{{ $post->descripcion }}</p>
             </div>
+
+            @auth
+                @if( $post->user_id === auth()->user()->id )
+                    <form 
+                        action={{ route('posts.destroy', $post ) }}
+                        method="POST"
+                        >
+
+                        @method('DELETE')
+                        @csrf
+                        <input
+                            type="submit"
+                            value="Eliminar publicacion"
+                            class="bg-red-500 hover:bg-red-600 p-2 rounded text-white font-bold mt-10 cursor-pointer"
+                        />
+                    </form>
+                @endif
+
+            @endauth
+
         </div>
 
         <div class="md:w-1/2 p-5">
