d273aaf9080f58adc0d42ffcf974c40bca9ae9c1
alexhDev2808 <colores.2808.8000@gmail.com>
2024-05-04 11:40:08 -0600

    Creando la pag principal(Home)

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\ComentarioController;
 use App\Http\Controllers\FollowerController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\PerfilController;
@@ -22,9 +23,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('main');
-});
+Route::get('/', HomeController::class)->name('home');
 
 Route::get('/register', [RegisterController::class,'index'])->name('register'); // mostrar vista
 Route::post('/register', [RegisterController::class,'store']);
