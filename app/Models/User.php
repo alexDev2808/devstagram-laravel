ff560565abfa609ce6fcea14e9ea2582bcbfa52a
alexhDev2808 <colores.2808.8000@gmail.com>
2024-05-04 11:11:53 -0600

    Condicion para mostrar botones para seguir o dejar de seguir

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,5 +57,11 @@ class User extends Authenticatable
         return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
     }
 
+    // comprobar si un usuario ya sigue a otro
+    public function siguiendo(User $user) {
+        return $this->followers->contains($user->id);
+    }
+
+
     // almacena los que seguimos
 }
